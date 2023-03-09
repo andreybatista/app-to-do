47eee4afae06e300585cab67012df151a5e32ac9
andreybatista <andreybatista131@gmail.com>
2023-03-09 18:40:11 -0300

    add form

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,61 @@
+import { useEffect, useState } from 'react';
+import { Image, View, TextInput, TouchableOpacity, FlatList, Alert, Button, Modal, Text } from 'react-native';
+
+
+// import logo from '../../assets/logo.png'
+
+import { styles } from './styles';
+
+export function Home() {
+  const [isFocused, setIsFocused] = useState(false);
+
+  // const DEFAULT_IMAGE = Image.resolveAssetSource(DefaultImage).uri;
+
+
+  return (
+    <>
+      <View style={styles.containerHeader}>
+
+        <Image source={require('../../assets/logo.png')} />
+
+
+      </View >
+      <View style={styles.containerBody}>
+
+        <View style={styles.containerForm}>
+          <TextInput
+            style={[styles.input, isFocused && styles.onFocusInput]}
+            placeholderTextColor="#808080"
+            onFocus={() => setIsFocused(true)}
+            onBlur={() => setIsFocused(false)}
+            placeholder='Adicione uma nova tarefa'
+          />
+
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>
+              +
+            </Text>
+          </TouchableOpacity>
+        </View >
+
+        <View style={styles.containerListHeader}>
+
+          <View style={styles.boxHeaderList}>
+            <Text style={[styles.textHeaderList, { color: "#4EA8DE" }]}>
+              Criadas
+            </Text>
+            <Text style={styles.textCountList}>0</Text>
+          </View>
+
+          <View style={styles.boxHeaderList}>
+            <Text style={[styles.textHeaderList, { color: "#8284FA" }]}>
+              Concluídas
+            </Text>
+            <Text style={styles.textCountList}>0</Text>
+          </View>
+
+        </View>
+      </View >
+    </>
+  )
+}
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,81 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  containerHeader: {
+    height: 173,
+    backgroundColor: "#0D0D0D",
+    justifyContent: "center",
+    alignItems: "center",
+    // padding: 24,
+  },
+  containerBody: {
+    flex: 1,
+    backgroundColor: "#1A1A1A",
+    paddingLeft: 24,
+    paddingRight: 24,
+  },
+  containerForm: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 4,
+    marginTop: -27,
+  },
+  input: {
+    fontSize: 16,
+    lineHeight: 140,
+    color: "#F2F2F2",
+    backgroundColor: "#262626",
+    borderWidth: 1,
+    borderColor: "#0D0D0D",
+    borderRadius: 6,
+    padding: 16,
+    height: 54,
+    flex: 1,
+  },
+  button: {
+    height: 54,
+    width: 54,
+    backgroundColor: "#1E6F9F",
+    borderRadius: 6,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonText: {
+    fontSize: 16,
+    color: "#F2F2F2",
+  },
+  onFocusInput: {
+    borderColor: "#5E60CE",
+  },
+  containerListHeader: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 32,
+    paddingBottom: 20,
+  },
+  boxHeaderList: {
+    flexDirection: "row",
+    gap: 8,
+  },
+  textHeaderList: {
+    fontSize: 14,
+    lineHeight: 17,
+    fontWeight: "700",
+    color: "#F2F2F2",
+  },
+  textCountList: {
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    textAlign: "center",
+    width: 25,
+    padding: 2,
+    backgroundColor: "#333333",
+    borderRadius: 999,
+    color: "#D9D9D9",
+    fontWeight: "700",
+    fontSize: 12,
+    lineHeight: 15,
+  },
+});
